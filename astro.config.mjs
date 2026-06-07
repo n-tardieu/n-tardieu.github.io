90b38479ab39e44851aebd423f6301ac5fae6a4f
Nicolas Tardieu <ntardieu.contact@gmail.com>
2026-06-07 18:58:46 +0200

    fix:  add support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://n-tardieu.github.io',
+    redirects: {
+        '/coheria/privacy-fr': '/privacy#fr',
+        '/coheria/privacy-en': '/privacy#en',
+    },
 });
